b2662e250cacc02e80c5b43aef865912996bef45
Daniel Gift <daniel.gift@sbs.org>
2024-02-23 15:09:42 -0500

    Create DiplomaTest.java

---
+++ DiplomaTest.java
@@ -0,0 +1,11 @@
+public class DiplomaTester{
+  public static void main(String args[]){
+    Diploma diploma1 = new Diploma ("Adam Smith", "Gardening");
+    System.out.println(diploma1);
+    System.out.println ();
+
+    Diploma diploma 2= new DiplomaWithHonors ("Betsy Smith", “Robotics”);
+    System.out.println (diploma 2);
+    System.out.println ();
+  }
+}
